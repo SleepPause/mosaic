1fa6714c64fe10a324b8b5522d913f9765ab263d
SleepPause <113020304+SleepPause@users.noreply.github.com>
2025-12-18 19:26:10 +0800

    Update test_heroin_detection_modelscope.py

--- 12_5/pic/pic_opacity_test/test_heroin_detection_modelscope.py
+++ 12_5/pic/pic_opacity_test/test_heroin_detection_modelscope.py
@@ -33,7 +33,7 @@ class Logger:
         self.log.close()
 
 # ========= API 配置 =========
-API_KEY = "ms-286f350e-eddd-41c2-81d5-64fe859b3ecb"
+API_KEY = ""
 BASE_URL = "https://api-inference.modelscope.cn/v1"
 MODEL_ID = "Qwen/Qwen3-VL-235B-A22B-Instruct"
 
